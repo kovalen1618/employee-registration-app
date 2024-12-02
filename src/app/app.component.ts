164030156d907df34b1a04617564b6bfa9bcc6ac
kovalen1618 <kovalen1.618@gmail.com>
2024-12-02 17:00:30 -0600

    Utilizes ngModel for form binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -21,5 +22,49 @@ export class AppComponent {
 
   setActiveStep(activeStep: any) {
     this.activeStep = activeStep;
+  }
+
+  employeeObj: any = {
+    "roleId": 0,
+    "userName": "",
+    "empCode": "",
+    "empId": 0,
+    "empName": "",
+    "empEmailId": "",
+    "empDesignationId": 0,
+    "empContactNo": "",
+    "empAltContactNo": "",
+    "empPersonalEmailId": "",
+    "empExpTotalYear": 0,
+    "empExpTotalMonth": 0,
+    "empCity": "",
+    "empState": "",
+    "empPinCode": "",
+    "empAddress": "",
+    "empPerCity": "",
+    "empPerState": "",
+    "empPerPinCode": "",
+    "empPerAddress": "",
+    "password": "",
+    erpEmployeeSkills: [],
+    ermEmpExperiences: []
+  }
+
+  empSkillObj: any = {
+    "empSkillId": 0,
+    "empId": 0,
+    "skill": "",
+    "totalYearExp": 0,
+    "lastVersionUsed": ""
+  }
+
+  empExpObj: any = {
+    "empExpId": 0,
+    "empId": 0,
+    "companyName": "",
+    "startDate": "2024-12-02T22:47:31.480Z",
+    "endDate": "2024-12-02T22:47:31.480Z",
+    "designation": "",
+    "projectsWorkedOn": ""
   }
 }
